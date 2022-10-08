8e41b537e49eebe403a09d5c01d51b272b90f0f5
Ian Pieragostini <ian.pieragostini@gmail.com>
2022-10-08 17:37:14 +0700

    debugging (disabled)

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,15 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
 #[wasm_bindgen]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -57,7 +66,15 @@ impl Universe {
                 let idx = self.get_index(row, col);
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
-
+                /*
+                                log!(
+                                    "cell[{}, {}] is initially {:?} and has {} live neighbors",
+                                    row,
+                                    col,
+                                    cell,
+                                    live_neighbors
+                                );
+                */
                 let next_cell = match (cell, live_neighbors) {
                     // Rule 1: Any live cell with fewer than two live neighbours
                     // dies, as if caused by underpopulation.
@@ -75,6 +92,8 @@ impl Universe {
                     (otherwise, _) => otherwise,
                 };
 
+                //log!("    it becomes {:?}", next_cell);
+
                 next[idx] = next_cell;
             }
         }
@@ -84,6 +103,8 @@ impl Universe {
 
     #[allow(clippy::new_without_default)]
     pub fn new() -> Universe {
+        utils::set_panic_hook();
+
         let width = 64;
         let height = 64;
 
